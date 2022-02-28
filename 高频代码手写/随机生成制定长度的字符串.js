1726cd1ed1c4edca39736f2226623aaf902551f2
liupeng <liupeng20162341@163.com>
2022-02-28 18:02:00 +0800

    生成随机字符串

---
+++ 高频代码手写/随机生成制定长度的字符串.js
@@ -0,0 +1,13 @@
+// 随机生成制定长度的字符串
+function randomString(n) {
+    let str = 'abcdefghijklmnopqrstuvwxyz9876543210';
+    let tmp = '',
+        i = 0,
+        l = str.length;
+    for (i = 0; i < n; i++) {
+        tmp += str.charAt(Math.floor(Math.random() * l));
+    }
+    return tmp;
+}
+
+console.log(randomString(8))
