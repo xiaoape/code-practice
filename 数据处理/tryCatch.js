5be801567f1a0ece59ec2531590e230475429e52
bruceliu <bruceliu@nswap.com>
2022-04-11 19:13:43 +0800

    feat: try catch增加参数处理

--- 数据处理/tryCatch.js
+++ 数据处理/tryCatch.js
@@ -1,11 +1,27 @@
 // 优雅的处理try catch
 async function errorCaptured(asyncFunc) {
+    if (typeof asyncFunc !== 'function') {
+        throw new TypeError('Error')
+    }
     try {
-        let res = await asyncFunc();
+        let arg = [...arguments].slice(1)
+        let res = await asyncFunc(...arg);
         return [null, res]
     } catch (e) {
         return [e, null]
     }
 }
 
-let [err, res] = await errorCaptured(asyncFunc)
+// 测试
+const test = async (a, b) => {
+    let result = await a + b
+    // throw new TypeError('Error')
+    return result
+}
+
+async function run() {
+    let [err, res] = await errorCaptured(test, 2,3)
+    if(err) console.log(err, 'error')
+    console.log(res, 'resss')
+}
+run()
