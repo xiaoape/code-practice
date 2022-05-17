72875031e59dab71e932c76a6db10ae3ea0b08a1
bruceliu <bruceliu@nswap.com>
2022-05-17 14:52:47 +0800

    feat: try catch修改

--- 数据处理/tryCatch.js
+++ 数据处理/tryCatch.js
@@ -1,4 +1,5 @@
 // 优雅的处理try catch
+// 方式一
 async function errorCaptured(asyncFunc) {
     if (typeof asyncFunc !== 'function') {
         throw new TypeError('Error')
@@ -9,6 +10,36 @@ async function errorCaptured(asyncFunc) {
         return [null, res]
     } catch (e) {
         return [e, null]
+    }
+}
+
+// 方式一的问题是如果传入的是对象里的一个方法，而这个方法里会依赖this，那么就会导致this指向错误的问题
+// 所以最好是将函数进行bind处理后传入方式一
+
+// 方式二能保证方式一的问题
+async function errorCapturedTwo(obj, fucName) {
+    if (obj) {
+        if (typeof obj[fucName] !== 'function') {
+            throw new TypeError('Error')
+        }
+        try {
+            let arg = [...arguments].slice(2)
+            let res = await obj[fucName](...arg);
+            return [null, res]
+        } catch (e) {
+            return [e, null]
+        }
+    } else {
+        if (typeof fucName !== 'function') {
+            throw new TypeError('Error')
+        }
+        try {
+            let arg = [...arguments].slice(1)
+            let res = await fucName(...arg);
+            return [null, res]
+        } catch (e) {
+            return [e, null]
+        }
     }
 }
 
