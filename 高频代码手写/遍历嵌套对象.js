c0dad1aef6fc8f7c9b83c355c07b133f1b76c4fb
bruceliu <bruceliu@nswap.com>
2022-05-24 11:33:27 +0800

    feat: 代码测试

--- 高频代码手写/遍历嵌套对象.js
+++ 高频代码手写/遍历嵌套对象.js
@@ -14,22 +14,26 @@ const getData = () => {
     }, obj)
     return newArr
 }
-console.log(getData());
-
-// 第二种实现方式
-const obj = {
-    a: {
-        b: {
-            c: 666
-        }
-    }
-}
-
-var str = 'a.b.c';
-const getData = (obj, str) => {
+// 方式二
+const getData2 = (obj, str) => {
     str.split('.').forEach(element => {
         obj = obj[element]
     })
     return obj;
 }
-console.log(getData(obj, str));
+console.log(getData(obj, str)); // 666
+console.log(getData2(obj, str)); // 666
+
+// 自测
+const test = (obj, str) => {
+    const arr = str.split('.')
+    let realObj = obj
+    for(let i = 0;i< arr.length;i++) {
+        if (typeof realObj[arr[i]] === 'object') {
+            realObj = realObj[arr[i]]
+        } else {
+            return realObj[arr[i]]
+        }
+    }
+}
+console.log(test(obj, str)) // 666
