0bfd2c0b44ab105772f20c7a0783fe8f5f3c3c60
bruceliu <bruceliu@nswap.com>
2022-05-27 14:36:04 +0800

    feat: 二维数组测试

--- 数据结构和算法/二维数组.js
+++ 数据结构和算法/二维数组.js
@@ -16,4 +16,15 @@ console.log(arrTrans(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 // 二维数组转化为一维数组
 const arr=[[1,2,3],[3,4],[5]];
-console.log([].concat.apply([],arr));
+console.log([].concat.apply([],arr)); // [1,2,3,3,4,5,]
+
+console.log(arr.flat(Infinity)) // [1,2,3,3,4,5,]
+
+
+// 3. 判断当前数组时一维数组还是多维数组
+let arr2 = [[0, 1], [2, 3], [4, 5]];
+function isDeep(arr){
+  return arr.some(item=> item instanceof Array);
+}
+console.log(isDeep(arr2))
+// 多维返回true，一维返回false
