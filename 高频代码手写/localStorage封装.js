8464b44530cdedee79f667ffaa6f47227bde7347
bruceliu <bruceliu@nswap.com>
2023-03-17 13:46:17 +0800

    使用chatGpt修改代码

--- 高频代码手写/localStorage封装.js
+++ 高频代码手写/localStorage封装.js
@@ -2,54 +2,37 @@
 // localStorage/sessionStorage默认只能存储字符串，而实际开发中，
 // 我们往往需要存储的数据多为对象类型，那么这里我们就可以在存储时
 //利用json.stringify()将对象转为字符串，而在取缓存时，只需配合json.parse()转回对象即可
-const isClient = typeof window !== 'undefined'
+const isClient = typeof window === 'object' && window !== null
 
 export function setItem(key, val) {
-  isClient && localStorage.setItem(key, JSON.stringify(val) || {})
+  if (isClient) {
+    localStorage.setItem(key, JSON.stringify(val))
+  }
 }
 
 export function getItem(key) {
-  let storageContent
   try {
-    storageContent = isClient && JSON.parse(localStorage.getItem(key)) || {} //TODO 坑，值为0，返回{}
+    const storageContent = isClient && JSON.parse(localStorage.getItem(key))
+    return storageContent ?? null // or undefined, or throw an error
   } catch (error) {
-    storageContent = {}
+    console.error(error)
+    return null // or undefined, or throw an error
   }
-
-  return storageContent
 }
 
 export function deleteItem(key) {
   try {
-    storageContent = isClient && JSON.parse(localStorage.getItem(key)) || {}
-    if (Object.keys(storageContent).length > 0) {
+    const storageContent = isClient && JSON.parse(localStorage.getItem(key))
+    if (storageContent && key in storageContent) { // check for falsy values and key existence
       delete storageContent[key]
       setItem(key, storageContent)
     }
   } catch (error) {
-
+    console.error(error)
+    // throw error or do something else
   }
 }
 
 export function removeItem(key) {
   localStorage.removeItem(key)
-}
-
-export function setSessionItem(key, val) {
-  isClient && sessionStorage.setItem(key, val)
-}
-
-export function getSessionItem(key) {
-  let storageContent
-  try {
-    storageContent = isClient && sessionStorage.getItem(key)
-  } catch (error) {
-    storageContent = {}
-  }
-
-  return storageContent
-}
-
-export function removeSessionItem(key) {
-  isClient && sessionStorage.removeItem(key)
 }
