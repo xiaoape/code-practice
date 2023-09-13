9f19a3429f4e767c7a7d5758e33ae16b8312fd42
liupeng <bruceliu@nswap.com>
2023-09-13 22:43:47 +0800

    防抖扩展

--- 高频代码手写/debounce.js
+++ 高频代码手写/debounce.js
@@ -6,13 +6,84 @@
 const debounce = (func, wait = 50) => {
     // 缓存一个定时器id
     let timer = null
+    let result = null
     // 这里返回的函数是每次用户实际调用的防抖函数
     // 如果已经设定过定时器了就清空上一次的定时器
     // 开始一个新的定时器，延迟执行用户传入的方法
     return function (...args) {
         if (timer) clearTimeout(timer)
+        // 注意这里是箭头函数的写法，不能换成普通函数的写法
         timer = setTimeout(() => {
-            func.apply(this, args)
+            result = func.apply(this, args)
         }, wait)
+        return result
     }
 }
+
+// 扩展： 可立即执行的防抖函数
+function debounce(func, wait, immediate) {
+
+    var timeout, result;
+
+    return function () {
+        var context = this;
+        var args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow) result = func.apply(context, args)
+        }
+        else {
+            timeout = setTimeout(function(){
+                result = func.apply(context, args)
+            }, wait);
+        }
+
+        return result;
+    }
+}
+
+// 扩展： 我希望能取消 debounce 函数，比如说我 debounce 的时间间隔是 10 秒钟，immediate 为 true，这样的话，
+// 我只有等 10 秒后才能重新触发事件，现在我希望有一个按钮，点击后，取消防抖，这样我再去触发，就可以又立刻执行啦
+
+
+// 第六版
+function debounce(func, wait, immediate) {
+
+    var timeout, result;
+
+    var debounced = function () {
+        var context = this;
+        var args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow) result = func.apply(context, args)
+        }
+        else {
+            timeout = setTimeout(function(){
+                result = func.apply(context, args)
+            }, wait);
+        }
+        return result;
+    };
+
+    debounced.cancel = function() {
+        clearTimeout(timeout);
+        timeout = null;
+    };
+
+    return debounced;
+}
+
+// 参考链接：https://juejin.cn/post/6844903480239325191
