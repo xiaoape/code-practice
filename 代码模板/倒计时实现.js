c22fe9c2496e2eccfed8a5c0693c4df4e30b7a26
bruceliu <bruceliu@nswap.com>
2023-09-18 15:48:12 +0800

    redux

--- 代码模板/倒计时实现.js
+++ 代码模板/倒计时实现.js
@@ -1,7 +1,8 @@
 // 使用 requestAnimationFrame实现倒计时
 // setInterval是宏任务，定时器不准，遇到线程被阻塞后，会有很大的误差
 // 使用setTimeout实现，通过计算setInterval延迟了多久，然后动态的设置下次setTimeout触发的时间去修正误差
-// setTimout能明显降低误差，但是setTimeout依旧是宏任务，依旧会被阻塞，如果使用动画依旧可能会掉帧
+// setTimout能明显降低误差，但是setTimeout依旧是宏任务，依旧会被阻塞，如果使用动画依旧可能会掉帧，而且不好
+// 的地方是在校准时间的过程中，为了快速追赶落后的时间，时间跳动太快了，导致体验不太好，体感上感觉这时间不准
 
 // setTimeout，setInterval属于JS引擎，RAF属于GUI引擎，RAF不受JS线程阻塞
 // 注意单纯的使用RAF依然会存在误差
@@ -54,3 +55,4 @@ useEffect(() => {
 }, [duration])
 
 // 参考链接：https://juejin.cn/post/7022636375136534565
+// https://juejin.cn/post/7026735190634414087
--- 开源实现/实现简易的redux.js
+++ 开源实现/实现简易的redux.js
@@ -1,3 +1,74 @@
 // 实现简易的redux
 
-// 略
+// actionType.js
+const actionType = {
+    INSREMENT: 'INSREMENT',
+    DECREMENT: 'DECREMENT',
+    RESET: 'RESET'
+  }
+  export default actionType
+  
+  // actions.js
+  import actionType from './actionType'
+  const add = (num) => ({
+      type: actionType.INSREMENT,
+      payload: num
+  })
+  
+  const dec = (num) => ({
+      type: actionType.DECREMENT,
+      payload: num
+  })
+  
+  const getList = (data) => ({
+      type: actionType.GETLIST,
+      payload: data
+  })
+  export {
+      add,
+      dec,
+      getList
+  }
+  
+  // reducer.js
+  function init(initialCount) {
+    return {
+      count: initialCount,
+      total: 10,
+      user: {},
+      article: []
+    }
+  }
+  
+  function reducer(state, action) {
+    switch (action.type) {
+      case actionType.INSREMENT:
+        return {count: state.count + action.payload};
+      case actionType.DECREMENT:
+        return {count: state.count - action.payload};
+      case actionType.RESET:
+        return init(action.payload);
+      default:
+        throw new Error();
+    }
+  }
+  
+  export { init, reducer }
+  
+  // redux.js
+  import React, { useReducer, useContext, createContext } from 'react'
+  import { init, reducer } from './reducer'
+  
+  const Context = createContext()
+  const Provider = (props) => {
+    const [state, dispatch] = useReducer(reducer, props.initialState || 0, init);
+      return (
+        <Context.Provider value={{state, dispatch}}>
+          { props.children }
+        </Context.Provider>
+      )
+  }
+  
+  export { Context, Provider }
+  
+// 参考链接：https://juejin.cn/post/6844904074433789959#heading-4
